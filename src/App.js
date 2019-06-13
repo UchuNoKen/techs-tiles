4655da7fd164e712af8e444d4a0e011db11372fe
UchuNoKen <xen.o.fite@gmail.com>
2019-06-13 13:42:05 -0400

    create LibraryList cmpnt, reducer

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { Header } from "./components/common";
+import LibraryList from "./components/LibraryList";
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>App</View>
+      <View>
+        <Header headerText="Tech Tiles" />
+        <LibraryList />
+      </View>
     </Provider>
   );
 };
