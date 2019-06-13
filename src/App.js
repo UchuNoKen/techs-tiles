bb0f6511c40735de9022f00d1b8d7994616a5ba4
UchuNoKen <xen.o.fite@gmail.com>
2019-06-13 12:39:22 -0400

    wire up redux

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@
 
 import React from "react";
 import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
 const App = () => {
-  return <View>App</View>;
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>App</View>
+    </Provider>
+  );
 };
 
 export default App;
