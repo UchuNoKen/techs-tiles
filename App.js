7877204199154a3e275eb1d8b424577e757ef61e
UchuNoKen <xen.o.fite@gmail.com>
2019-06-13 12:28:56 -0400

    initial boilerplate commit

---
+++ App.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
